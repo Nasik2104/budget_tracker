29998199a534c6de7bbf298ae7fab1e341839035
unknown <gavrisnazar981@gmail.com>
2024-02-02 21:32:58 +0200

    weather

---
+++ weater/app/routes/main.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import render_template, request, redirect
+from flask.blueprints import Blueprint
+
+import requests
+
+bp = Blueprint("default", __name__)
+
+wea = "47ac3b0e8423e801dd60854c54b2f58f"
+
+@bp.route('/', methods=["GET"])
+def main_page():
+
+    value = request.args.get('q')
+    weather = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?q={value}&appid={wea}&units=metric"
+    )
+    data = weather.json()
+
+    print(data)
+
+    return render_template("main.html", data=data)
