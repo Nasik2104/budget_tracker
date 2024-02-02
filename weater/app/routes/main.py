6045df1495450c650015fc2800b2457e3a7f0f8f
unknown <gavrisnazar981@gmail.com>
2024-02-02 21:59:38 +0200

    weather

--- weater/app/routes/main.py
+++ weater/app/routes/main.py
@@ -11,13 +11,9 @@ wea = "47ac3b0e8423e801dd60854c54b2f58f"
 
 @bp.route('/', methods=["GET"])
 def main_page():
-
     value = request.args.get('q')
     weather = requests.get(
         f"http://api.openweathermap.org/data/2.5/weather?q={value}&appid={wea}&units=metric"
     )
     data = weather.json()
-
-    print(data)
-
     return render_template("main.html", data=data)
