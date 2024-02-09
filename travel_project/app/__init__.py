4a87b76d4fee7606d20efdef7411d14b3424a1e1
unknown <gavrisnazar981@gmail.com>
2024-02-09 14:57:37 +0200

    9

--- travel_project/app/__init__.py
+++ travel_project/app/__init__.py
@@ -15,11 +15,4 @@ def create_app():
     from app import models
     create_db()
 
-    @app.errorhandler(403)
-    @app.errorhandler(404)
-    @app.errorhandler(405)
-    @app.errorhandler(500)
-    def handler(e):
-        return render_template('error.html', code=e.code)
-
     return app
