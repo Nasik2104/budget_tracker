1b1f08a88c0038358bd580823c1f3c84bbcc8fb4
unknown <gavrisnazar981@gmail.com>
2024-02-09 14:59:29 +0200

    9

--- lesson_9_homework/app/__init__.py
+++ lesson_9_homework/app/__init__.py
@@ -6,7 +6,6 @@ from app.database import create_db, drop_db, Session
 
 def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
-    app.config["SECRET_KEY"] = os.urandom(12).hex()
     from app.routes import default_bp
 
     app.register_blueprint(default_bp, url_prefix="/")
